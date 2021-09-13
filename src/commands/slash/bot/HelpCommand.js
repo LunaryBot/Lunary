838ce96b401046f8b4f3e6bfab4a7456e89e4abb
JVOPINHO <jvopinho.dev@gmail.com>
2021-09-12 23:41:24 -0300

    Multi lang no comando de help e botões no help

--- src/commands/slash/bot/HelpCommand.js
+++ src/commands/slash/bot/HelpCommand.js
@@ -19,24 +19,43 @@ module.exports = class HelpCommand extends Command {
 
     async run(ctx) {
         const Bae = await this.client.users.fetch("452618703792766987")
-
-        const embed = new Discord.MessageEmbed()
-        .setColor("#A020F0")
-        .setAuthor(`Lunary ajuda • ${version}`, this.client.user.avatarURL({ dynamic: true, format: "png", size: 1024 }), this.client.config.links.website.home)
-        .setDescription(`<:YaY:835190406899695697> **|** Olá ${ctx.author}, eu sou a Lunar, sou um bot com um grande sonho,\nMeu foco principal é manter seu servidor organizado, ajudando na moderação e administração. Este é meu painel de ajuda, aqui você poderá descobrir no que eu posso ajudar a você e o servidor.`)
-        .addField(":clipboard: Minha lista de comandos", `> ${this.client.config.links.website.commands}`)
-        .addField("<:Duvida:835272427516461146> Precisando de ajuda? Pergunte no meu servidor de suporte!", `> ${this.client.config.links.website.support}`)
-        .addField(":envelope_with_arrow: Que tal votar em mim para me ajudar a  chegar em outras pessoas?", `> ${this.client.config.links.website.vote}`)
-        .addField(":love_letter: Que tal me adicionar em seu servidor?", `> [${this.client.config.links.website.invite}](${this.client.generateOauth2({
+        const invite = this.client.generateOauth2({
             permissions: 8n,
             redirect: `${this.client.config.links.website.callback}`,
             scopes: ["bot", "applications.commands", "guilds", "identify"],
-        })})`)
-        .addField(":gear: Me configure pela minha dashboard", `> ${this.client.config.links.website.dashboard.me}`)
-        .setFooter(`• Fui criada e desenvolvida por: ${Bae.tag}`, Bae.avatarURL({ dynamic: true, format: "png", size: 1024 }))
+        })
+
+        const embed = new Discord.MessageEmbed()
+        .setColor("#A020F0")
+        .setAuthor(`${ctx.t("help/title")} • ${version}`, this.client.user.avatarURL({ dynamic: true, format: "png", size: 1024 }), this.client.config.links.website.home)
+        .setDescription(`<:YaY:835190406899695697> **|** ${ctx.t("help/description", { author: ctx.author.toString() })}`)
+        .addField(`:clipboard: ${ctx.t("help/links/commands")}`, `> ${this.client.config.links.website.commands}`)
+        .addField(`<:Duvida:835272427516461146> ${ctx.t("help/links/support")}`, `> ${this.client.config.links.website.support}`)
+        .addField(`:love_letter: ${ctx.t("help/links/add")}`, `> [${this.client.config.links.website.invite}](${invite})`)
+        .addField(`:envelope_with_arrow: ${ctx.t("help/links/vote")}`, `> ${this.client.config.links.website.vote}`)
+        .addField(`:gear: ${ctx.t("help/links/dashboard")}`, `> ${this.client.config.links.website.dashboard.me}`)
+        .setFooter(`• ${ctx.t("help/created_by")} ${Bae.tag}`, Bae.avatarURL({ dynamic: true, format: "png", size: 1024 }))
+        .setThumbnail("https://imgur.com/iacDuXp.png")
+
+        const components = new Discord.MessageActionRow()
+        .addComponents([
+            new Discord.MessageButton()
+            .setStyle("LINK")
+            .setURL(invite)
+            .setLabel(ctx.t("help/buttons/invite_me")),
+            new Discord.MessageButton()
+            .setStyle("LINK")
+            .setURL(this.client.config.links.support)
+            .setLabel(ctx.t("help/buttons/support")),
+            new Discord.MessageButton()
+            .setStyle("LINK")
+            .setURL(this.client.config.links.website.home)
+            .setLabel(ctx.t("help/buttons/website"))
+        ])
 
         await ctx.interaction.reply({
-            embeds: [embed]
+            embeds: [embed],
+            components: [components]
         })
     }
 }
