21ee1487b2b2bedc4dd36f35e1003a92d41206d9
JVOPINHO <jvopinho.dev@gmail.com>
2021-09-27 00:27:34 -0300

    Comando de avatar...

--- src/commands/slash/utilities/UserAvatarSubCommand.js
+++ src/commands/slash/utilities/UserAvatarSubCommand.js
@@ -15,8 +15,8 @@ module.exports = class UserAvatarSubCommand extends SubCommand {
      */
 
     async run(ctx) {
-        const userID = `${ctx.interaction.options.getString("user")}`.replace(/<@!?(\d{18})>/, "$1")
-        const user = /^\d{18}$/.test(userID) ? await this.client.users.fetch(userID).catch(() => {}) : null
+        const userID = ctx.interaction.options.getString("user")?.replace(/<@!?(\d{18})>/, "$1")
+        const user = !userID || userID == ctx.author.id ? ctx.author : (/^\d{18}$/.test(userID) ? await this.client.users.fetch(userID).catch(() => {}) : null)  
         
         if(!user) return await ctx.interaction.reply({
             embeds: [
@@ -24,14 +24,29 @@ module.exports = class UserAvatarSubCommand extends SubCommand {
             ]
         }).catch(() => {})
 
-        const avatar = user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 });
+        const avatar = user.displayAvatarURL({ dynamic: true, format: "png" });
         
         await ctx.interaction.reply({
             embeds: [
                 new Discord.MessageEmbed()
-                .setImage(avatar)
-                .setTitle(`:frame_photo: ${user.tag}`)
+                .setAuthor(user.username, `https://cdn.discordapp.com/emojis/${this.client.config.devs.includes(user.id) ? "844347009543569449" : "832083303627620422"}.png?size=128`)
+                .setDescription(`${asl(128)} | ${asl(256)} | ${asl(512)} | ${asl(1024)} | ${asl(2048)}`)
+                .setImage(avatar + "?size=1024")
+                .setColor(this.client.config.devs.includes(user.id) ? "#FFFAFA" : "#A020F0")
+            ],
+            components: [
+                new Discord.MessageActionRow()
+                .addComponents([
+                    new Discord.MessageButton()
+                    .setStyle("LINK")
+                    .setURL(avatar + "?size=1024")
+                    .setLabel(ctx.t("user_avatar:texts.downloadImage"))
+                ])
             ]
         })
+        
+        function asl(size) {
+            return `[x${size}](${avatar}?size=${size})`
+        }
     }
 }
