8c2bc7e3076225a5f5ee1e8d71a0b989f302d0af
JVOPINHO <jvopinho.dev@gmail.com>
2022-01-04 11:32:00 -0300

    Fix ~> RoleInfoSubCommand

--- src/commands/slash/utilities/RoleInfoSubCommand.js
+++ src/commands/slash/utilities/RoleInfoSubCommand.js
@@ -29,13 +29,12 @@ module.exports = class RoleInfoSubCommand extends SubCommand {
             .setThumbnail(role.iconURL({ dynamic: true }) || null)
 			.addField(`:bookmark: ${ctx.t('role_info:texts.roleMention')}:`, `\`${role.toString()}\``, true)
 			.addField(':open_file_folder: ID', `\`${role.id}\``, true)
-            .addField('||\n||', '||\n||', true)
-            .addField(`:busts_in_silhouette: ${ctx.t('role_info:texts.roleMembers')}:`, `${role.members.size}`, true)
+            .addField(`:busts_in_silhouette: ${ctx.t('role_info:texts.roleMembers', { size: role.members.size })}`, `||\n||`, true)
             .addField(`:eyes: ${ctx.t('role_info:texts.roleHoisted')}`, `${ctx.t(`general:booleans.${!!role.hoist}`)}`, true)
             .addField(`:bell: ${ctx.t('role_info:texts.roleMentionable')}`, `${ctx.t(`general:booleans.${!!role.mentionable}`)}`, true)
-            .addField(`:robot: ${ctx.t('role_info:texts.roleManaged')}`, `${ctx.t(`general:booleans.${!!role.managed}`)}`, true)
+            .addField(`:robot: ${ctx.t('role_info:texts.roleIntegration')}`, `${ctx.t(`general:booleans.${!!role.managed}`)}`, true)
 		    .addField(`:calendar_spiral: ${ctx.t('role_info:texts.createdAt')}`, `<t:${Math.floor((role.createdTimestamp + 3600000) / 1000.0)}> (<t:${Math.floor((role.createdTimestamp + 3600000) / 1000.0)}:R>)`)
-            .addField(`:shield: ${ctx.t('role_info:texts.permissions')}`, `${role.permissions.toArray().map(x => ctx.t(`permissions:${x}`)).join(', ')}`)
+            .addField(`:shield: ${ctx.t('role_info:texts.permissions')}`, `${role.permissions.toArray().map(x => `\`${ctx.t(`permissions:${x}`)}\``).join(', ')}`)
 		
         ctx.interaction.reply({
 			embeds: [embed],
