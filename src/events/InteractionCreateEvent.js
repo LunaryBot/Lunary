773fc73a6fa12c8d36bbca56020a39296285978c
JVOPINHO <jvopinho.dev@gmail.com>
2022-02-10 23:31:22 -0300

    Update 'quick fix' in `InteractionCreateEvent`

--- src/events/InteractionCreateEvent.js
+++ src/events/InteractionCreateEvent.js
@@ -1,5 +1,11 @@
 const Event = require('../structures/Event.js');
-const { Interaction, CommandInteraction, MessageEmbed, Collection, User, ContextMenuInteraction } = require('../lib');
+const {
+	Interaction, 
+	CommandInteraction, 
+	MessageEmbed, 
+	Collection, 
+	ContextMenuInteraction,
+} = require('../lib');
 const ContextCommand = require('../structures/ContextCommand.js');
 const Command = require('../structures/Command.js');
 
@@ -25,10 +31,10 @@ module.exports = class InteractionCreateEvent extends Event {
 	 */
 	async run(interaction) {
 		if (interaction.isCommand()) return this.executeCommand(interaction);
-		if (interaction.isContextMenu()) this.executeUserCommand(interaction);
+		if (interaction.isContextMenu()) return this.executeUserCommand(interaction);
 		if (interaction.isAutocomplete()) {
-			if (interaction.commandName == 'ban') return this.autocompleteBan(interaction);
-			if (interaction.commandName == 'adv') return this.autocompleteAdv(interaction);
+			this.client.int = interaction;
+			return this.autocomplete(interaction);
 		}
 	}
 
@@ -201,134 +207,27 @@ module.exports = class InteractionCreateEvent extends Event {
 	 *
 	 * @param {CommandInteraction} interaction
 	 */
-	async autocompleteBan(interaction) {
-		let output = [];
-		if (interaction.guild) {
-			const getData = async () => {
-				this.client.logger.log('Request para auto complete de ban.', {
-					cluster: true,
-					date: true,
-					key: `Shard ${interaction.guild.shardId}`,
-				});
-
-				const bans = [...(await interaction.guild.bans.fetch()).values()]?.map(ban => ban.user);
-				const requestTime = Date.now();
-				return {
-					validTime: requestTime + 3 * 1000 * 60,
-					requestTime,
-					bans,
-				};
-			};
-
-			/**
-			 * @type {{
-			 * validTime: number,
-			 * requestTime: number,
-			 * bans: User[]
-			 * }}
-			 */
-			let data = this.bansCache.get(interaction.guildId);
-			if (!data || data.validTime <= Date.now()) {
-				data = await getData();
-				this.bansCache.set(interaction.guildId, data);
-			}
-
-			const input = interaction.options
-				.get('user')
-				?.value?.toLowerCase()
-				.replace(/<@!?(\d{17,19})>/, '$1');
-
-			const arr = data.bans?.map(user => {
-				return {
-					name: user.tag,
-					value: user.id,
-				};
-			});
-
-			output = input ? arr.filter(ban => ban.name.toLowerCase().includes(input) || ban.value.toLowerCase().includes(input)) : arr;
+	async autocomplete(interaction) {
+		/**
+		 * @type {Command}
+		 */
+		let command = this.client.commands.slash.find(c => c.name == interaction.commandName.toLowerCase());
+		if (!command) return;
+		if (command.subcommands?.length) {
+			let subcommand;
+			if (interaction.options._group) subcommand = command.subcommands.find(c => c.name == interaction.options._group);
+			subcommand = (subcommand || command).subcommands?.find(c => c.name == interaction.options._subcommand || c.name == interaction.options._group) || subcommand;
+			if (subcommand) command = subcommand;
 		}
+		if (!command) return;
 
-		return this.client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 8,
-				data: {
-					choices: [...output].slice(0, 25),
-				},
-			},
-		});
-	}
+		/**
+		 * @type {Command.AutoComplete}
+		 */
+		const autocomplete = command.autocomplete;
 
-	/**
-	 *
-	 * @param {CommandInteraction} interaction
-	 */
-	async autocompleteAdv(interaction) {
-		let output = [];
-		if (interaction.guild) {
-			const getData = async () => {
-				this.client.logger.log("Request para auto complete de adv's.", {
-					cluster: true,
-					date: true,
-					key: `Shard ${interaction.guild.shardId}`,
-				});
-
-				const logs = await this.client.LogsDB.ref().once('value');
-				const advs = Object.entries(logs.val() || {})
-					.map(function ([k, v], i) {
-						const data = JSON.parse(Buffer.from(v, 'base64').toString('ascii'));
-						return data;
-					})
-					.filter(x => x.server == interaction.guild.id && x.type == 4)
-					?.sort((a, b) => b.date - a.date)
-					.map(x => x.id)
-					.filter(x => x);
-
-				const requestTime = Date.now();
-				return {
-					validTime: requestTime + 1 * 1000 * 60,
-					requestTime,
-					advs,
-				};
-			};
-
-			/**
-			 * @type {{
-			 * validTime: number,
-			 * requestTime: number,
-			 * advs: string[]
-			 * }}
-			 */
-			let data = this.advsCache.get(interaction.guildId);
-			if (!data || data.validTime <= Date.now()) {
-				data = await getData();
-				this.advsCache.set(interaction.guildId, data);
-			}
-
-			const input = interaction.options.get('id')?.value?.toLowerCase();
-
-			const arr = data.advs?.map(id => {
-				return {
-					name: id,
-					value: id,
-				};
-			});
-
-			output = input
-				? arr.filter(adv => {
-						return adv.value.toLowerCase().includes(input);
-				  })
-				: arr;
-		}
-
-		return this.client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 8,
-				data: {
-					choices: [...output].slice(0, 25),
-				},
-			},
-		});
+		if (!autocomplete) return;
+		
+		autocomplete.run(interaction);
 	}
 };
-
-// Tipos de Interactions https://discord.com/developers/docs/interactions/slash-commands#interaction-object-interaction-request-type
