638e57667fa2a5edee06014abb83f5dde554f045
JVOPINHO <jvopinho.dev@gmail.com>
2022-03-06 07:41:41 -0300

    Alert about slash commands

--- src/events/MessageCreateEvent.js
+++ src/events/MessageCreateEvent.js
@@ -21,18 +21,15 @@ module.exports = class MessageCreateEvent extends Event {
 			if (perms && !perms.has('SEND_MESSAGES')) return;
 		}
 
-		let GuildsDB = message.guild ? await this.client.db.ref().once('value') : null;
-		if (GuildsDB) GuildsDB = GuildsDB.val() || {};
-
 		const defaultprefix = this.client.config.prefix;
-		const regexp = new RegExp(`^(${`${message.guild ? new ObjRef(GuildsDB).ref(`Servidores/${message.guild.id}/prefix`).val() || defaultprefix : defaultprefix}`.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')}|<@!?${this.client.user.id}>)( )*`, 'gi');
+		const regexp = new RegExp(`^(${`${defaultprefix}`.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')}|<@!?${this.client.user.id}>)( )*`, 'gi');
 		if (!message.content.match(regexp)) return;
 
 		try {
 			const args = message.content.replace(regexp, '').trim().split(/ +/g);
 			if (!args.length) return;
 			const commandName = args.shift()?.toLowerCase();
-			const command = this.client.commands.vanilla.find(c => c.name == commandName || (Array.isArray(c.aliases) && c.aliases.includes(commandName)));
+			const command = this.client.commands.slash.find(c => c.name == commandName || (Array.isArray(c.aliases) && c.aliases.includes(commandName)));
 			if (!command) return;
 			if (message.guild) {
 				const perms = message.channel.permissionsFor(this.client.user.id);
@@ -43,27 +40,40 @@ module.exports = class MessageCreateEvent extends Event {
 					if (!perms.has('ATTACH_FILES')) return message.reply(`> Eu preciso de permissão de \`Anexar arquivos\``);
 				}
 			}
+			
+			if(commandName != 'eval') message.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor('#A020F0')
+						.setTitle('<:verde_alingua:899326455274676225> Slash!!!')
+						.setDescription(`<:ASwephyCutecuteDONOTSTEAL_DFS:901578115317383199> Oiiiee, ${message.author.toString()}, parece que você está tentando executar um de meus comandos por mensagem, você sabia que em **Abril de 2022** todos os bots serão forçados a migrar para Slash Commands(Comandos de \`/\`)?
+						Se quiser executar esse comando, utilize \`/${command.name}\`(Slash Command). **Caso eles não estejam aparecendo em seu servidor você pode [reutilizar meu convite](${this.client.generateOauth2({
+							permissions: 8n,
+							redirect: `${this.client.config.links.website.callback}`,
+							scopes: ['bot', 'applications.commands', 'guilds', 'identify'],
+							guild: message.guild?.id,
+						})})**.
+						Caso você queira saber um "pouco mais" sobre o acesso de conteúdo de mensagens, e o motivo de nós, bots, estarmos fazendo esta mudança, você pode ler o [anúncio oficial do Discord](https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots)`)
+				]
+			})
 
-			let UsersDB = await this.client.UsersDB.ref().once('value');
-			UsersDB = UsersDB.val() || {};
+			const _command = this.client.commands.vanilla.find(c => c.name == commandName || (Array.isArray(c.aliases) && c.aliases.includes(commandName)));
 
-			const ctx = new ContextCommand(
-				{
-					client: this.client,
-					message: message,
-					args: args,
+			if (!_command) return;
+
+			const context = new ContextCommand({
+				client: this.client,
+					message,
 					guild: message.guild,
 					channel: message.channel,
 					user: message.author,
-					command: command,
-					slash: false,
-					prefix: message.content.replace(message.content.replace(regexp, ''), ''),
+					command: _command,
+					slash: true,
 					dm: !Boolean(message.guild),
-				},
-				{ guildsDB: GuildsDB, usersDB: UsersDB },
-			);
+					args,
+			}, {});
 
-			await command.run(ctx);
+			_command.run(context);
 		} catch (e) {
 			message
 				.reply({
