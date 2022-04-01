687f4198b2cd653617edced3b835a6dde4ac6102
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-01 14:21:19 -0300

    Added `Utils.highestPosition`

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,3 +1,5 @@
+import Eris, { Member } from "eris";
+
 class Utils {
     public static formatSizeUnits(bytes: number | string): string {
         if (typeof bytes === 'string') {
@@ -30,6 +32,18 @@ class Utils {
 
         return bytes;
     };
+
+    public static highestPosition(member1: Eris.Member, member2: Eris.Member) {
+        if (member1.id == member2.id || member1.guild.ownerID == member1.id) { return true };
+        if(member1.guild.ownerID == member2.id) { return false };
+
+        const roles = [ ...member1.guild.roles.values() ].sort((a, b) => b.position - a.position);
+
+        member1.roles.sort((a, b) => roles.findIndex(role => role.id == a) - roles.findIndex(role => role.id == b));
+        member2.roles.sort((a, b) => roles.findIndex(role => role.id == a) - roles.findIndex(role => role.id == b));
+
+        return roles.findIndex(role => role.id == member1.roles[0]) > roles.findIndex(role => role.id == member2.roles[0]);
+    }
 }
 
 export default Utils;
