f762eadcb6d891092b161cb7620bf3336492f1a6
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-08 23:25:33 -0300

    New router `/{guildID}/members/{memberID}`

--- src/structures/server/routers/GuildsRouter.ts
+++ src/structures/server/routers/GuildsRouter.ts
@@ -2,6 +2,22 @@ import BaseRouter from '../../BaseRouter';
 import { Router } from 'express';
 import Server from '../Server';
 
+const guildObjString = `{
+    id: guild.id,
+    name: guild.name,
+    icon: guild.iconURL,
+    roles: guild.roles.map((role) => role.toJSON()),
+    channels: guild.channels.map((channel) => {
+        const json = channel.toJSON();
+
+        delete json.guild;
+        delete json.permissionOverwrites;
+        delete json.messages;
+
+        return json;
+    }),
+};`
+
 class GuildsRouter extends BaseRouter {
     constructor(server: Server) {
         super({
@@ -14,10 +30,20 @@ class GuildsRouter extends BaseRouter {
             res.send('Guilds');
         });
 
-        this.router.get('/:id', async(req, res) => {
-            const id = req.params.id;
+        this.router.get('/:guildID', async(req, res) => {
+            const guildID = req.params.guildID;
 
-            const d = await this.getGuild(id);
+            const d = await this.getGuild(guildID);
+
+            const { status, ...data } = d;
+
+            res.status(status).json(data);
+        });
+
+        this.router.get('/:guildID/members/:userID', async(req, res) => {
+            const { guildID, userID } = req.params;
+
+            const d = await this.getMember(guildID, userID);
 
             const { status, ...data } = d;
 
@@ -25,46 +51,63 @@ class GuildsRouter extends BaseRouter {
         });
     }
 
-    private async getGuild(id: string) {
-        const evalResults: any[] = [];
+    private async getMember(guildID: string, userID: string) {
+        const results: any[] = await this.clusterManager.eval(`(async() => {
+            const guild = this.client.guilds.get('${guildID}');
 
-            this.clusterManager.clusters.forEach((cluster) => 
-                evalResults.push(
-                    this.clusterManager.eval(`(async() => {
-                        const guild = this.client.guilds.get('${id}');
+            if(!guild) { return; }
 
-                        if(guild) {
-                            return {
-                                id: guild.id,
-                                name: guild.name,
-                                icon: guild.iconURL,
-                                roles: guild.roles.map((role) => role.toJSON()),
-                                channels: guild.channels.map((channel) => {
-                                    const json = channel.toJSON();
+            const member = guild.members.get('${userID}');
 
-                                    delete json.guild;
-                                    delete json.permissionOverwrites;
-                                    delete json.messages;
+            if(member) {
+                console.log('member found');
+                const json = member.toJSON();
 
-                                    return json;
-                                }),
-                            };
-                        } else {
-                            return null;
-                        };
-                    })()`, cluster)
-                )
-            );
+                json.permissions = Number(member.permissions?.allow) || 0;
+                
+                json.guild = ${guildObjString};
 
-            const results = await Promise.all(evalResults);
-
-            const guild = results.find((result) => result !== null);
-
-            if(!guild) {
-                return { status: 498, message: 'Guild not found'};
+                return json;
             } else {
-                return { status: 200, ...guild };
-            }
+
+                console.log('Member not found');
+                return null;
+            };
+        })()`);
+
+        if(results.filter((result) => result !== undefined).length === 0) {
+            return { status: 498, message: 'Guild not found'};
+        }
+
+        const member = results.find((result) => result !== null && result !== undefined);
+
+        if(!member) {
+            return { status: 498, message: 'Member not found'};
+        } else {
+            return { status: 200, ...member };
+        }
+    }
+
+    private async getGuild(id: string) {
+        const results: any[] = await this.clusterManager.eval(`(async() => {
+            const guild = this.client.guilds.get('${id}');
+
+            if(guild) {
+                return ${guildObjString};
+            } else {
+                return null;
+            };
+        })()`);
+
+        // const results = await Promise.all(evalResults);
+
+        const guild = results.find((result) => result !== null);
+
+        if(!guild) {
+            return { status: 498, message: 'Guild not found'};
+        } else {
+            return { status: 200, ...guild };
+        }
     }
 }
 
