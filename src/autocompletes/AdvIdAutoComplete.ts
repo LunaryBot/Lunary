bb8bc1eb0054082abd70e03eabf994c44f059226
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-26 09:48:54 -0300

    Updated `AdvIdAutoComplete`

--- src/autocompletes/AdvIdAutoComplete.ts
+++ src/autocompletes/AdvIdAutoComplete.ts
@@ -4,20 +4,42 @@ import LunarClient from '../structures/LunarClient';
 import CommandInteractionOptions from '../utils/CommandInteractionOptions';
 
 class AdvIdAutoComplete extends AutoComplete {
+    public declare cache: Map<string, string[]>;
     constructor(client: LunarClient) {
         super(client, {
-            cacheClearInterval: 0,
+            cacheClearInterval: 3 * 1000 * 60,
         });
     }
 
     public async run(interaction: Eris.AutocompleteInteraction, options: CommandInteractionOptions): Promise<any> {
-        interaction.result([
-            {
-                name: 'test',
-                value: 'test',
-            }
-        ])
+        const { value: input } = options.focused as Eris.InteractionDataOptionsString;
+        const advs = await this.getData(interaction.guildID as string);
+        
+        const output = input
+            ? advs?.filter(adv => adv.toLowerCase().includes(input.toLowerCase()))
+            : advs;
+        
+        interaction.result(output?.map(adv => ({ name: adv, value: adv })) || []);
     }
+
+    public async getData(guildID: string) {
+        if(this.cache.has(guildID)) return this.cache.get(guildID);
+
+        const logs = await this.client.dbs.getLogs();
+		const advs: string[] = Object.entries(logs || {})
+		    .map(function ([k, v], i) {
+				const data = JSON.parse(Buffer.from(v, 'base64').toString('ascii'));
+				return {...data, id: k};
+			})
+			.filter(x => x.server == guildID && x.type == 4)
+			?.sort((a, b) => b.date - a.date)
+			.map(x => x.id)
+			.filter(x => x);
+
+        this.cache.set(guildID, advs);
+
+		return advs;
+    } 
 }
 
 export default AdvIdAutoComplete;
