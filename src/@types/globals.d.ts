e19cbaa63b9b76c1c95c917cd2f14a3f3248be0a
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-30 23:27:55 -0300

    Added more String tools

--- src/@types/globals.d.ts
+++ src/@types/globals.d.ts
@@ -5,5 +5,7 @@ declare global {
 
     interface String {
         shorten(length: number): string;
+        toTitleCase(): string;
+        checkSimilarityStrings(string: string): number;
     }
 }
