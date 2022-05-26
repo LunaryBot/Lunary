c2c4b5db3bbed9c732d3771aa3a54dcfc382ce4c
JVOPINHO <jvopinho.dev@gmail.com>
2022-05-26 14:42:10 -0300

    Adding `/users/cache` router

--- src/structures/server/routers/UsersRouter.ts
+++ src/structures/server/routers/UsersRouter.ts
@@ -15,6 +15,60 @@ class UsersRouter extends BaseRouter {
 
             res.json(users);
         });
+
+        this.router.get('/cache', async(req, res) => {
+            const users = req.body.users;
+            
+            let results = await this.clusterManager.eval(`(async() => {
+                const users = ${JSON.stringify(users)};
+
+                const usersData = [];
+                
+                users.map((userID) => {
+                    const user = this.client.users.get(userID);
+
+                    if(user) {
+                        return usersData.push(user.toJSON());
+                    } else {
+                        return null;
+                    };
+                });
+
+                    return usersData;
+            })()`).then(async results => {
+                results = results.flat();
+
+                const usersNotFound = users.filter((userID: string) => {
+                    return !results.find((user) => user.id === userID);
+                });
+    
+                if(usersNotFound.length > 0) {
+                    const usersNotFoundResults = await this.clusterManager.eval(`(async() => {
+                        const users = ${JSON.stringify(usersNotFound)};
+        
+                        const usersData = [];
+    
+                        for(const userID of users) {
+                            const user = await this.client.getRESTUser(userID);
+    
+                            console.log('Force', userID, user?.username);
+        
+                            if(user) {
+                                usersData.push(user.toJSON());
+                            }
+                        };
+    
+                        return usersData;
+                    })()`, 0);
+    
+                    results.push(...usersNotFoundResults);
+                }
+
+                return results.flat(Infinity);
+            });
+
+            res.json(results);
+        });
     }
 
     private async getUsers() {
