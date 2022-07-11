03e2ed3ed70dabd64b6d4cd771fe31ea004339e2
jvopinho <jvopinho.dev@gmail.com>
2022-07-11 19:54:46 -0300

    Removing `express`

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
 import 'dotenv/config';
-import './tools/Logger'
-
-import express from 'express';
-import http from 'http';
+import './tools/Logger';
 
 import Client from './structures/Client';
 
-new Client(process.env.DISCORD_CLIENT_TOKEN);
+const client = new Client(process.env.DISCORD_CLIENT_TOKEN);
 
-
-
-const app = express();
-const httpServer = http.createServer(app);
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hellow World!');
-});
-
-httpServer.listen(process.env.PORT, () => {
-    logger.info(`Http Server is running on port ${process.env.PORT} (http://localhost:${process.env.PORT})`, { label: 'Http Server' })
-});
+client.init();
