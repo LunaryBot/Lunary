6441c7ac4d503d4833c2970994d26ced26d621cd
jvopinho <jvopinho.dev@gmail.com>
2022-07-11 19:15:03 -0300

    Updating Application structure

--- src/discord/Application.ts
+++ src/discord/Application.ts
@@ -6,49 +6,70 @@ import User from './User';
 import { TScope } from '../@types/discord.d';
 
 class Application {
-    public id: string;
-    public name: string;
-    public icon: string | null;
-    public description: string;
-    public rpc_origin?: Array<string>;
-    public bot_public: boolean;
-    public bot_require_code_grant: boolean;
-    public terms_of_service_url?: string;
-    public privacy_policy_url?: string;
-    public owner?: User;
-    public verify_key: string;
-    public team?: Team;
-    public guild_id?: string;
-    public slug?: string;
-    public cover_image?: string;
-    public flags?: number;
-    public tags?: Array<string>;
-    public install_params?: {
+	public id: string;
+	public name: string;
+	public icon: string | null;
+	public description: string;
+	public rpcOrigin?: Array<string>;
+	public botPublic: boolean;
+	public botRequireCodeGrant: boolean;
+	public termsOfServiceUrl?: string;
+	public privacyPolicyUrl?: string;
+	public owner: User;
+	public readonly verifyKey: string;
+	public team: Team;
+	public guildId?: string;
+	public slug?: string;
+	public coverImage?: string;
+	public flags?: number;
+	public tags?: [string, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?] | undefined;
+	public installParams?: {
         scopes: Array<TScope>;
         permissions: string;
     };
 
 	constructor(data: RESTGetAPIOAuth2CurrentApplicationResult) {
-        this.id = data.id;
+		this.id = data.id;
 
-        this.name = data.name;
+		this.name = data.name;
 
-        this.icon = data.icon;
+		this.icon = data.icon;
 
-        this.description = data.description;
+		this.description = data.description;
 
-        this.rpc_origin = data.rpc_origins;
+		this.rpcOrigin = data.rpc_origins;
 
-        this.bot_public = data.bot_public;
+		this.botPublic = data.bot_public;
 
-        this.bot_require_code_grant = data.bot_require_code_grant;
+		this.botRequireCodeGrant = data.bot_require_code_grant;
 
-        this.terms_of_service_url = data.terms_of_service_url;
+		this.termsOfServiceUrl = data.terms_of_service_url;
 
-        this.privacy_policy_url = data.privacy_policy_url;
+		this.privacyPolicyUrl = data.privacy_policy_url;
 
-        this.owner = data.owner ? new User(data.owner) : undefined;
-    }
+		this.owner = data.owner ? new User(data.owner) : undefined as any;
+
+		Object.defineProperty(this, 'verifyKey', {
+			enumerable: false,
+			writable: false,
+			value: data.verify_key,
+		});
+
+		this.team = data.team ? new Team(data.team) : undefined as any;
+
+		this.guildId = data.guild_id;
+
+		this.slug = data.slug;
+
+		this.coverImage = data.cover_image;
+
+		// @ts-ignore
+		this.flags = data.flags;
+
+		this.tags = data.tags;
+
+		this.installParams = data.install_params as any;
+	}
 }
 
 export default Application;
