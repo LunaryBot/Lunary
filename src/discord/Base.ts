f83f499cd38a0e0fcf953982c4119ecb7c7955ce
jvopinho <jvopinho.dev@gmail.com>
2022-08-07 20:54:49 -0300

    Adding Base `Strucuture.raw`

--- src/discord/Base.ts
+++ src/discord/Base.ts
@@ -1,12 +1,21 @@
 class Structure<RawType = any> {
 	public client: LunaryClient;
-    
+	public raw: RawType;
+
 	constructor(client: LunaryClient, raw?: RawType) {
 		Object.defineProperty(this, 'client', {
 			writable: false,
 			value: client,
 			enumerable: false,
 		});
+
+		if(raw) {
+			Object.defineProperty(this, 'raw', {
+				writable: false,
+				value: raw,
+				enumerable: false,
+			});
+		}
 	}
 }
 
