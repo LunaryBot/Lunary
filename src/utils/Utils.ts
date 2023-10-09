df8317bb80eb4fb203bd7c641862e0aad560a658
João Pinho <jvopinho.dev@gmail.com>
2023-10-09 14:30:58 -0300

    Fixing Utils.highestPosition

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -86,20 +86,23 @@ class Utils {
 	}
 
 	public static highestPosition(member1: Member, member2: Member, guild: Guild) {
-		if(guild.ownerId == member1.id) { 
-			return true; 
-		};
+		// if(guild.ownerId == member1.id) { 
+		// 	return true; 
+		// };
 
-		if(member1.id == member2.id || guild.ownerId == member2.id) { 
-			return false; 
-		};
+		// if(member1.id == member2.id || guild.ownerId == member2.id) { 
+		// 	return false; 
+		// };
 
 		const roles = [ ...guild.roles.values() ].sort((a, b) => a.position - b.position);
 
 		member1.roles.sort((a, b) => roles.findIndex(role => role.id == a) - roles.findIndex(role => role.id == b));
 		member2.roles.sort((a, b) => roles.findIndex(role => role.id == a) - roles.findIndex(role => role.id == b));
 
-		return roles.findIndex(role => role.id == member1.roles[0]) >= roles.findIndex(role => role.id == member2.roles[0]);
+		const member1HighsetRoleIndex = roles.findIndex(role => role.id == member1.roles[0]);
+		const member2HighsetRoleIndex = roles.findIndex(role => role.id == member2.roles[0]);
+
+		return member1HighsetRoleIndex < member2HighsetRoleIndex;
 	}
 }
 
